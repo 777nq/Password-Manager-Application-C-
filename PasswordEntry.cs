d47d4d09e76bfe4b2c32b33f28c5fab9beca27e8
Thagib <92385122+777nq@users.noreply.github.com>
2023-09-03 20:08:49 +0300

    PasswordEntry.cs

---
+++ PasswordEntry.cs
@@ -0,0 +1,10 @@
+// PasswordEntry.cs
+using System;
+
+[Serializable]
+public class PasswordEntry
+{
+    public string Website { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
